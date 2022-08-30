ef979863a8f01916a4b03d20df6f037f12ef0290
josecis0216 <josecis0216@gmail.com>
2022-08-29 18:23:11 -0600

    added vueitfy grids to home and about page

--- src/pages/default/AboutPage.vue
+++ src/pages/default/AboutPage.vue
@@ -1,49 +1,69 @@
 <template>
-  <div>
-    <h2>Past Projects</h2>
-    <base-card>
-      <p>
-        Most of the projects here, I have completed following along a course on
-        Udemy. It was taught by Max from Academind, and I feel he's one of the
-        best instructors on udemy covering Vuejs. I have another website that I
-        started to build for a friend that was starting a epoxy floor business
-        that never really took off. Please play around with the projects and
-        check out the links to my other projects. Thanks for taking a look!
-      </p>
-      <div class="grid-container">
-        <div>
-            <h4>Epoxy Floors Website</h4>
-            <a href="https://epicfloors.netlify.app/" alt="link to epic site for epoxy floors" target="_blank">Epic Flooring</a>
-        </div>
-        <div>
-            <h4>Coaches List Website</h4>
-            <a href="https://vue-practice-88f8e.web.app/coaches" alt="link to coaches site" target="_blank">Coaches</a>
-        </div>
-      </div>
-    </base-card>
-    <div class="grid-container">
-      <base-card>
-        <h4>Monster Slayer Project</h4>
-        <p>
-          This was my first project completed through the Vuejs course on udemy.
-          It is a game that allows a player to attack a monster and inflict
-          stronger damage upon using the 'strong attack' button. Likewise, the
-          monster can attack the player. The game was built using Vuejs and
-          includes a lot of css styles to get the healthbar working.
-        </p>
-        <monster-slayer></monster-slayer>
-      </base-card>
-      <base-card>
-        <h4>Add Task Project</h4>
-        <p>
-          Another project that I created following the Vuejs course. This one is
-          more simple but it taught me how to use props, and be able to
-          dynamically show/hide elements using the virutal dom.
-        </p>
-        <test-component></test-component>
-      </base-card>
-    </div>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="12">
+        <h2>Past Projects</h2>
+        <base-card>
+          <p>
+            Most of the projects here, I have completed following along a course
+            on Udemy. It was taught by Max from Academind, and I feel he's one
+            of the best instructors on udemy covering Vuejs. I have another
+            website that I started to build for a friend that was starting a
+            epoxy floor business that never really took off. Please play around
+            with the projects and check out the links to my other projects.
+            Thanks for taking a look!
+          </p>
+          <div class="grid-container">
+            <div>
+              <h4>Epoxy Floors Website</h4>
+              <a
+                href="https://epicfloors.netlify.app/"
+                alt="link to epic site for epoxy floors"
+                target="_blank"
+                >Epic Flooring</a
+              >
+            </div>
+            <div>
+              <h4>Coaches List Website</h4>
+              <a
+                href="https://vue-practice-88f8e.web.app/coaches"
+                alt="link to coaches site"
+                target="_blank"
+                >Coaches</a
+              >
+            </div>
+          </div>
+        </base-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="12" lg="6">
+        <base-card>
+          <h4>Monster Slayer Project</h4>
+          <p>
+            This was my first project completed through the Vuejs course on
+            udemy. It is a game that allows a player to attack a monster and
+            inflict stronger damage upon using the 'strong attack' button.
+            Likewise, the monster can attack the player. The game was built
+            using Vuejs and includes a lot of css styles to get the healthbar
+            working.
+          </p>
+          <monster-slayer></monster-slayer>
+        </base-card>
+      </v-col>
+      <v-col cols="12" sm="12" lg="6">
+        <base-card>
+          <h4>Add Task Project</h4>
+          <p>
+            Another project that I created following the Vuejs course. This one
+            is more simple but it taught me how to use props, and be able to
+            dynamically show/hide elements using the virutal dom.
+          </p>
+          <test-component></test-component>
+        </base-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
--- src/pages/default/HomePage.vue
+++ src/pages/default/HomePage.vue
@@ -1,43 +1,54 @@
 <template>
-  <div>
-    <div class="grid-container" style="background-color: antiquewhite">
-      <div class="title">
-        <h5>Web Developer</h5>
-        <h1>
-          Jose <br />
-          Cisneros
-        </h1>
-      </div>
-      <div style="max-height: 400px; max-width: 400px; margin-top: 50px;">
-        <v-img
-          :src="require('../../assets/headshot.jpg')"
-          alt="headshot of Jose Cisneros"
-          class="rounded-circle"
-          max-height="auto"
-          max-width="auto"
-        />
-      </div>
-    </div>
-    <base-card style="max-width: 60rem;">
-      <h2>Welcome to my page!</h2>
-      <p class="introP">
-        I am a full stack web developer. I am most familiar with vb.net, but
-        have currently been working with Vuejs. I plan to learn more react,
-        however I plan to continue improving my skillset with Vuejs and Vb.net.
-        I am also familiar with SQL and relational databases.
-      </p>
-      <p>
-        Check out my
-        <a href="www.github.com/josecis0216" target="_blank">GitHub Page</a>
-      </p>
-    </base-card>
-  </div>
+  <v-container>
+    <v-row style="background-color: antiquewhite">
+      <v-col cols="12" sm="12" lg="6">
+        <div class="title">
+          <h5>Web Developer</h5>
+          <h1>
+            Jose <br />
+            Cisneros
+          </h1>
+        </div>
+      </v-col>
+      <v-col cols="12" sm="12" lg="6">
+        <div class="img-styles">
+          <v-img
+            :src="require('../../assets/headshot.jpg')"
+            alt="headshot of Jose Cisneros"
+            class="rounded-circle"
+            max-height="auto"
+            max-width="auto"
+          />
+        </div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="12">
+        <base-card style="max-width: 60rem">
+          <h2>Welcome to my page!</h2>
+          <p class="introP">
+            I am a full stack web developer. I am most familiar with vb.net, but
+            have currently been working with Vuejs. I plan to learn more react,
+            however I plan to continue improving my skillset with Vuejs and
+            Vb.net. I am also familiar with SQL and relational databases.
+          </p>
+          <p>
+            Check out my
+            <a href="www.github.com/josecis0216" target="_blank">GitHub Page</a>
+          </p>
+        </base-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped>
+.v-container {
+  max-width: 100%;
+}
 .title {
   text-align: left;
-  max-width: 1005px;
+  max-width: 260px;
   margin: auto;
 }
 .title h5 {
@@ -45,6 +56,12 @@
 }
 .title h1 {
   font-size: 64px;
+}
+.img-styles {
+  max-height: 400px;
+  max-width: 400px;
+  margin-top: 50px;
+  margin-bottom: 30px;
 }
 .grid-container {
   display: grid;
