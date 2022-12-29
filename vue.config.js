20201616e54c2b9618a25ec4883b48d4c803c4ab
josecis0216 <26750468+josecis0216@users.noreply.github.com>
2022-12-29 14:21:31 -0700

    changed name of project

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
         config
             .plugin('html')
             .tap(args => {
-                args[0].title = "My Vue App";
+                args[0].title = "Jose Cisneros - Portfolio";
                 return args;
             })
     },
